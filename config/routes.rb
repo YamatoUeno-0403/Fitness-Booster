91f158df2b7ac5cb0bda713ae801f03d121d272f
EC2 Default User <ec2-user@ip-172-31-34-100.ap-northeast-1.compute.internal>
2021-09-12 03:54:11 +0000

    devise addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: :customers do
+    resources :posts
+    
+  end
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
 end
---
+++ test/controllers/customers/posts_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Customers::PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get customers_posts_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get customers_posts_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get customers_posts_new_url
+    assert_response :success
+  end
+
+end
